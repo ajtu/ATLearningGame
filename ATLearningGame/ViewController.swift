a4c88bc1a247cd603b5d5121e90b935fb5510187
Alvin Tu <tu.alvinj@gmail.com>
2021-01-23 22:58:15 -0500

    Remove UI function and add button actions for displaying left and right colors

--- ATLearningGame/ViewController.swift
+++ ATLearningGame/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
         //        view.backgroundColor = UIColor.red
         
         setupUI()
+        
     }
     
     
@@ -25,15 +26,56 @@ class ViewController: UIViewController {
         let FIRST_BUTTON_FRAME = CGRect(x: 0, y: 0, width: VIEW_WIDTH/2, height: VIEW_HEIGHT )
         let SECOND_BUTTON_FRAME = CGRect(x: VIEW_WIDTH/2, y: 0, width: VIEW_WIDTH/2, height: VIEW_HEIGHT )
         
-        addButton(with: UIColor.red, and: FIRST_BUTTON_FRAME )
-        addButton(with: UIColor.orange, and: SECOND_BUTTON_FRAME )
+//        addButton(with: UIColor.red, and: FIRST_BUTTON_FRAME )
+//        addButton(with: UIColor.orange, and: SECOND_BUTTON_FRAME )
         
-    }
-    func addButton(with color:UIColor, and frame:CGRect){
-        let redButton = UIButton(frame: frame)
-        redButton.backgroundColor = color
+        let redButton = UIButton(frame: FIRST_BUTTON_FRAME)
+        redButton.backgroundColor = UIColor.red
+        redButton.addTarget(self,
+        action: #selector(displayLeft),
+        for: .touchUpInside)
+        
+        let yellowButton = UIButton(frame: SECOND_BUTTON_FRAME)
+        yellowButton.backgroundColor = UIColor.yellow
+        yellowButton.addTarget(self,
+        action: #selector(displayRight),
+        for: .touchUpInside)
+        
         view.addSubview(redButton)
+        view.addSubview(yellowButton)
+
     }
-    
+    @objc func displayLeft(){
+        let VIEW_HEIGHT = view.frame.size.height
+        let VIEW_WIDTH = view.frame.size.width
+        let FIRST_BUTTON_FRAME = CGRect(x: VIEW_WIDTH/4, y: 0, width: VIEW_WIDTH/2, height: VIEW_HEIGHT )
+
+        let redLabel = UILabel(frame: FIRST_BUTTON_FRAME)
+//        yellowLabel.backgroundColor = UIColor.black
+        redLabel.text = "RED"
+        view.addSubview(redLabel)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            // code to remove your view
+            redLabel.removeFromSuperview()
+        }
+
+    }
+    @objc func displayRight(){
+        let VIEW_HEIGHT = view.frame.size.height
+        let VIEW_WIDTH = view.frame.size.width
+        let SECOND_BUTTON_FRAME = CGRect(x: VIEW_WIDTH/1.50, y: 0, width: VIEW_WIDTH/2, height: VIEW_HEIGHT )
+
+          let yellowLabel = UILabel(frame: SECOND_BUTTON_FRAME)
+  //        yellowLabel.backgroundColor = UIColor.black
+          yellowLabel.text = "YELLOW"
+
+          view.addSubview(yellowLabel)
+          DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+              // code to remove your view
+              yellowLabel.removeFromSuperview()
+          }
+
+    }
+
     
 }
