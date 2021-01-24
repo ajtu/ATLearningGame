ae106c199fc2cd5dfdf5c13bc8a0c05d156035fd
Alvin Tu <tu.alvinj@gmail.com>
2021-01-23 22:21:10 -0500

    Add starting frames and buttons

--- ATLearningGame/ViewController.swift
+++ ATLearningGame/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //        view.backgroundColor = UIColor.red
+        
+        setupUI()
     }
-
-
+    
+    
+    func setupUI(){
+        let VIEW_HEIGHT = view.frame.size.height
+        let VIEW_WIDTH = view.frame.size.width
+        let FIRST_BUTTON_FRAME = CGRect(x: 0, y: 0, width: VIEW_WIDTH/2, height: VIEW_HEIGHT )
+        let SECOND_BUTTON_FRAME = CGRect(x: VIEW_WIDTH/2, y: 0, width: VIEW_WIDTH/2, height: VIEW_HEIGHT )
+        
+        addButton(with: UIColor.red, and: FIRST_BUTTON_FRAME )
+        addButton(with: UIColor.orange, and: SECOND_BUTTON_FRAME )
+        
+    }
+    func addButton(with color:UIColor, and frame:CGRect){
+        let redButton = UIButton(frame: frame)
+        redButton.backgroundColor = color
+        view.addSubview(redButton)
+    }
+    
+    
 }
-
